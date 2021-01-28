993ce329df848fd43432fd2595b79aa8bbeb759a
roberto sanso <44288253+stefan08caxius@users.noreply.github.com>
2021-01-28 13:12:02 +0100

    connessione

--- index.js
+++ index.js
@@ -29,5 +29,8 @@ connection.connect(function(err) {
       .use(express.static(path.join(__dirname, 'public')))
       .set('views', path.join(__dirname, 'views'))
       .set('view engine', 'ejs')
+      .get('/', (req, res) => {
+              res.send('risp_tempo');
+        })
       .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 })
