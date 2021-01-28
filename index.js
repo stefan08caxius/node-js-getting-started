e9353ae71691890da4fb7888b2c44144e66f4679
roberto sanso <44288253+stefan08caxius@users.noreply.github.com>
2021-01-28 12:30:03 +0100

    connessione

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+
   .get('/cool', (req, res) => res.send({username: 'pippo'}))
+
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
