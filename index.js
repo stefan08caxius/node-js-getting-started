5256a67912e1b6fd612f8ca2eeec9b32abecd9c7
roberto sanso <44288253+stefan08caxius@users.noreply.github.com>
2021-01-28 12:31:24 +0100

    connessione

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
+const app = express();
 
-express()
+app
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
